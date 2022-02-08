26506a848ed2bf0c1d79733e747c172f49d9f2c6
Brittany Petersen <britt.petersen100@gmail.com>
2022-02-08 16:10:14 -0700

    added delete and get comments requests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -84,4 +84,45 @@ router.put('/:id', async (req, res) => {
 });
 
 
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const post = await Posts.findById(id)
+        if (post == null) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+            return;
+        } else {
+            const deletedPost = await Posts.remove(id);
+            res.json(deletedPost);
+        }
+    }
+    catch (error) {
+        res.status(500).json({
+            message: 'The post could not be removed'
+        })
+    }
+})
+
+router.get('/:id/comments', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const post = await Posts.findById(id);
+        if (!post) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        } else {
+            res.status(200).json(post)
+        }
+    }
+    catch (error) {
+        res.status(500).json({
+            message: 'The comments information could not be retrieved'
+        })
+    }
+})
+
+
 module.exports = router;
