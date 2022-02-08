89bc363cf5dc1b10d1b5569716e02ca7cdf3d140
Brittany Petersen <britt.petersen100@gmail.com>
2022-02-08 13:30:20 -0700

    added first get enpoint to get all posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,7 +1,20 @@
 // implement your posts router here
 const router = require('express').Router();
 
-// const Post = require('./posts-model');
+const Posts = require('./posts-model');
+
+router.get('/', (req, res) => {
+    console.log('my router is working!');
+    Posts.find(req.query)
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'The posts information could not be retrieved.'
+        })
+    })
+});
 
 
 module.exports = router;
